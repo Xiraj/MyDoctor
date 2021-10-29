d62e9086d4f816c1636a599a0613cfbb6a9b5ac4
Akbar Sirajuddin Hanif <akbarsirajuddinh@gmail.com>
2021-10-29 14:48:58 +0700

    add fonts and react-navigation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import Router from './router';
 
 const App = () => {
   return (
-    <View>
-      <Text>Welcome App JS</Text>
-    </View>
+    <NavigationContainer>
+      <Router />
+    </NavigationContainer>
   )
 }
 
 export default App;
-
-const styles = StyleSheet.create({})
---
+++ src/router/index.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+import { GetStarted, Login, Register, SplashScreen } from '../screens'
+
+const Stack = createStackNavigator();
+
+const Router = () => {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen 
+             name="SplashScreen" 
+             component={SplashScreen} 
+             options={{headerShown: false}} 
+            />
+            <Stack.Screen
+             name="GetStarted"
+             component={GetStarted}
+             options={{headerShown: false}} 
+            />
+            <Stack.Screen
+             name="Register"
+             component={Register}
+             options={{headerShown: false}} 
+            />
+            <Stack.Screen
+             name="Login"
+             component={Login}
+             options={{headerShown: false}} 
+            />
+        </Stack.Navigator>
+    )
+}
+
+
+export default Router;
